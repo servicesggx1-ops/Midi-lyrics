7f7f381478b87d2bc0c5c407fee39b9688de1815
servicesggx1-ops <289084588+servicesggx1-ops@users.noreply.github.com>
2026-09-16 16:03:27 -0400

    v21: chargeur ?banque= (banque Termux)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "midilyrics-v2";
+const CACHE = "ggx-v21-banque";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (e) => {
