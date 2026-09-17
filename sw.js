e0ff2ae82968f7fd50a37b9607ecc2bb0b6fb1c2
servicesggx1-ops <289084588+servicesggx1-ops@users.noreply.github.com>
2026-09-17 07:12:27 -0400

    mlfix: couches ML1+ML2 (pistes séparées, MID au vrai tempo, ZIP) · 2958 lignes [pixel-6]

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "midi-2026-09-17-ml2-2935l";
+const CACHE = "midi-2026-09-17-ml3-2958l";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (e) => {
